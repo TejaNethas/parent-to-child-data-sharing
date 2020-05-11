ce33d5e4452f58284d280c287c0af6090cc990c0
Teja_Nethas <saitejau12@gmail.com>
2020-05-11 20:23:09 +0530

    parent to child data sharing
    via Input decorator

---
+++ parent.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  template: `
+    <app-child [childMessage]="parentMessage"></app-child>
+  `,
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent{
+  localMessageType2:string = "Rohit input type message passing";
+  constructor() { }
+}
